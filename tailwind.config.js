49a158fa7aebbc0762d3f7d60af2851080902147
sharmashiv24251 <83074416+sharmashiv24251@users.noreply.github.com>
2023-08-31 07:49:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const { react } = require("@babel/types");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#ffa31a",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimYlw: "rgba(255, 163, 26, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};

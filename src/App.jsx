03d5823b000795e52cd8319cbc95ce886d1635e7
sharmashiv24251 <83074416+sharmashiv24251@users.noreply.github.com>
2023-08-31 07:51:54 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Buisness,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={` bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Buisness /> <Billing /> <CardDeal /> <Testimonials />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
